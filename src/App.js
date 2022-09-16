dd3b508a93ccee8132c2c7c017a955fad8abbd2f
Nishkarsh <nishkarsh2105@gmail.com>
2022-09-16 23:30:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Recommended from './components/Recommended/Recommended';
+import SearchPage from './components/SearchPage/SearchPage';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header />
+              <div className="app__mainpage">
+                <Sidebar />
+                <Recommended />
+              </div>
+            </>
+          } />
+
+          <Route path='/search/:searchQuery' element={
+            <>
+              <Header />
+              <div className="app__mainpage">
+                <Sidebar />
+                <SearchPage />
+              </div>
+            </>
+          } />
+
+          <Route path='/video/:videoId' element={
+            <>
+              <Header />
+              <div className="app__mainpage">
+                <Sidebar />
+                <VideoPlayer />
+              </div>
+            </>
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
